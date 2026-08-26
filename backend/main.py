d3b30f48757bde9aea57504af91cfd69706df2ca
9musa <99musahaider@gmail.com>
2026-08-26 14:15:33 +0500

    Setup resume text extraction

---
+++ backend/main.py
@@ -0,0 +1,79 @@
+import os, io
+import fitz, docx2txt
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from pydantic import BaseModel
+import uuid
+
+app = FastAPI()
+
+def validate(file_bytes: bytes, filename: str) -> tuple[bool, str]:
+    ext = filename.split(".")[-1].lower()
+
+    if ext == "pdf":
+        try:
+            doc = fitz.open(stream=file_bytes, filetype="pdf")
+            if doc.is_encrypted:
+                return False, "PDF is password protected."
+            if len(doc) == 0:
+                return False, "PDF has no pages."
+
+            text_check = "".join([page.get_text() for page in doc[:2]])
+            if not text_check.strip():
+                return False, "PDF has no readable text (likely a scanned image)."
+
+            return True, "Valid PDF."
+        except Exception as e:
+            return False, f"Corrupted or invalid PDF structure, {str(e)}"
+
+    elif ext == "docx":
+        try:
+            file_stream = io.BytesIO(file_bytes)
+            text_check = docx2txt.process(file_stream)
+            if not text_check.strip():
+                return False, "DOCX file is empty."
+
+            return True, "Valid DOCX."
+        except Exception as e:
+            return False, f"Corrupted or invalid DOCX structure: {str(e)}"
+
+    return False, f"Unsupported file extension: .{ext}"
+
+def extract_text(file_bytes: bytes, filename: str) -> str:
+    ext = filename.split(".")[-1].lower()
+
+    if ext == "pdf":
+        doc = fitz.open(stream=file_bytes, filetype="pdf")
+        return "".join([page.get_text() for page in doc]).strip()
+        
+    elif ext == "docx":
+        file_stream = io.BytesIO(file_bytes)
+        return docx2txt.process(file_stream).strip()
+        
+    return ""
+
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"} 
+
+@app.post("/superior")
+async def enhanceResume(
+    resume_file: UploadFile = File(),
+    job_title: str = Form(),
+):
+    file_bytes = await resume_file.read()
+    is_valid, msg = validate(file_bytes, resume_file.filename)
+    if not is_valid:
+        raise HTTPException(status_code=400, detail=msg)
+
+    parsed_text = extract_text(file_bytes, resume_file.filename)
+    
+
+    job_id = str(uuid.uuid4()) # temporary uuid for guests, will be saved on DB
+    
+    return {"job_id": job_id, "status": "processing"}
+
+@app.get("/superior/{job_id}")
+def get_superior_resume():
+    return
